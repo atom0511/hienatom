eca24868304297e8d780726486433ba5815d0d59
atom0511 <totrungatom@gmail.com>
2019-12-08 14:19:38 +0700

    audio2

--- routers/audioRouter.js
+++ routers/audioRouter.js
@@ -10,8 +10,28 @@ const {
 Router.get("/audio", async (req, res) => {
     console.log("get /audio");
     let data = await readData();
-    let randomData = data[Math.floor(Math.random() * data.length)];
-    res.render("audioPage", { link_audio: randomData.audio, css: "/audio.css" });
+    let randomNumber = Math.floor(Math.random() * data.length);
+    res.render("audioPage", { link_audio: data[randomNumber].audio, css: "/audio.css" });
+    console.log(randomNumber);
+    Router.get("/back", async (req, res) => {
+        console.log("get /back");
+        if (randomNumber === 0) {
+            randomNumber = data.length - 1;
+        } else {
+            randomNumber = randomNumber - 1;
+        }
+        res.render("audioPage", { link_audio: data[randomNumber].audio, css: "/audio.css" });
+    })
+    Router.get("/next", async (req, res) => {
+        console.log("get /next");
+        if (randomNumber === data.length - 1) {
+            randomNumber = 0;
+        } else {
+            randomNumber = randomNumber + 1;
+        }
+        res.render("audioPage", { link_audio: data[randomNumber].audio, css: "/audio.css" });
+    })
 })
+
 
 module.exports = Router;
